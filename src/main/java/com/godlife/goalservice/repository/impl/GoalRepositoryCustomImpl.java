3404032151a607772c0efa63e9eafea34232ea2a
fDevJc <fdevjc@gmail.com>
2022-10-15 23:11:21 +0900

    feat: GoalRepository 추가

---
+++ src/main/java/com/godlife/goalservice/repository/impl/GoalRepositoryCustomImpl.java
@@ -0,0 +1,6 @@
+package com.godlife.goalservice.repository.impl;
+
+import com.godlife.goalservice.repository.GoalRepositoryCustom;
+
+public class GoalRepositoryCustomImpl implements GoalRepositoryCustom {
+}
