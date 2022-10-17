c8d4d886207d6dd0d72c244a64d869bc77371778
fDevJc <fdevjc@gmail.com>
2022-10-17 21:23:14 +0900

    refactor: POST /goals 수정

--- src/main/java/com/godlife/goalservice/api/GoalController.java
+++ src/main/java/com/godlife/goalservice/api/GoalController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -33,7 +30,8 @@ public class GoalController {
     }
 
     @PostMapping("/goals")
-    public ResponseEntity<ApiResponse> createGoals(CreateGoalRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    public ResponseEntity<ApiResponse> createGoals(@RequestBody CreateGoalRequest request) {
+        log.info("request: {}", request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createPostSuccessResponse());
     }
 }
