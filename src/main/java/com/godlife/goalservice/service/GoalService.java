ba9252ce747e4cd216a2c49c477db1404e7dbc21
fDevJc <fdevjc@gmail.com>
2022-10-19 17:01:06 +0900

    feat: 목표추가 메서드 추가

--- src/main/java/com/godlife/goalservice/service/GoalService.java
+++ src/main/java/com/godlife/goalservice/service/GoalService.java
@@ -3,6 +3,7 @@ package com.godlife.goalservice.service;
 import com.godlife.goalservice.domain.Goal;
 import com.godlife.goalservice.repository.GoalRepository;
 import com.godlife.goalservice.service.dto.GoalServiceDto;
+import com.godlife.goalservice.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,9 @@ import java.util.stream.Collectors;
 public class GoalService {
     private final GoalRepository goalRepository;
 
-    public List<GoalServiceDto> getGoalsWithMindsetsByMethodAndCount(String method, Integer count) {
-        /* todo token 풀기 */
-        Long userId = 1L;
-        /* =============== */
-
-        List<Goal> goals = goalRepository.findByUserId(userId);
-
-        return goals.stream().map(goal -> GoalServiceDto.of(goal)).collect(Collectors.toList());
+    public List<GoalServiceDto> getGoalsWithMindsetsByMethodAndCount(String method, Integer count, String authorization) {
+        List<Goal> goals = goalRepository.findByUserId(getUserInfo(authorization).getUserId());
+        return goals.stream().map(GoalServiceDto::of).collect(Collectors.toList());
     }
 
     @Transactional
@@ -33,5 +29,21 @@ public class GoalService {
         Goal goal = goalServiceDto.toEntity();
         log.info("goal: {}", goal);
         goalRepository.save(goal);
+    }
+
+    public List<GoalServiceDto> getGoals(String authorization) {
+        Long userId = getUserInfo(authorization).getUserId();
+
+        List<Goal> goals = goalRepository.findByUserId(userId);
+
+        return goals.stream().map(GoalServiceDto::of).collect(Collectors.toList());
+    }
+
+    private static UserDto getUserInfo(String authorization) {
+        // todo auth-service 호출하여 jwt payload 담긴 유저정보 받아오기
+        UserDto userDto = new UserDto();
+
+
+        return userDto;
     }
 }
