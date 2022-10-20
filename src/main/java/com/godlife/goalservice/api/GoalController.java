b58fed04211919d2fa2ea9d46ec877e2f08fa005
fDevJc <fdevjc@gmail.com>
2022-10-20 18:11:54 +0900

    refactor: 불필요 주석 삭제

--- src/main/java/com/godlife/goalservice/api/GoalController.java
+++ src/main/java/com/godlife/goalservice/api/GoalController.java
@@ -9,19 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/*
+todo
+목표 추가 후 client 원하는 response 데이터 물어보기
+ */
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class GoalController {
     private final GoalService goalService;
 
-    /**
-     * 목표와 마인드셋 조회
-     * @param authorization JWT 서비스 토큰
-     * @param method mindset을 가져오는 방법 ( normal, random )
-     * @param count  mindset의 갯수
-     * @return
-     */
     @GetMapping("/goals/mindsets")
     public ResponseEntity<ApiResponse> getGoalsWithMindsets(@RequestHeader("Authorization") String authorization,
                                                             @RequestParam(value = "method", defaultValue = "normal") String method,
@@ -30,27 +28,16 @@ public class GoalController {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.createGetSuccessResponse(goalService.getGoalsWithMindsetsByMethodAndCount(method, count, authorization)));
     }
 
-    /**
-     * 목표 추가
-     *
-     * @param request
-     * @return
-     */
     @PostMapping("/goals")
-    public ResponseEntity<ApiResponse> createGoal(@RequestBody CreateGoalRequest request) {
+    public ResponseEntity<ApiResponse> createGoal(@RequestHeader("Authorization") String authorization,
+                                                  @RequestBody CreateGoalRequest request) {
         log.info("request: {}", request);
-        goalService.createGoal(request.toGoalServiceDto());
-        //TODO 목표 추가 후 client 원하는 response 데이터 물어보기
+        goalService.createGoal(authorization, request.toGoalServiceDto());
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createPostSuccessResponse());
     }
 
-    /**
-     * 목표 조회
-     * @param authorization JWT 서비스 토큰
-     * @return
-     */
     @GetMapping("/goals")
-    public ResponseEntity<ApiResponse> getGoals(@RequestHeader(value = "Authorization") String authorization) {
+    public ResponseEntity<ApiResponse> getGoals(@RequestHeader("Authorization") String authorization) {
         log.info("Header/Authorization: {}", authorization);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.createGetSuccessResponse(goalService.getGoals(authorization)));
     }
