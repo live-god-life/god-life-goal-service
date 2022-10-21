f5c6f5868374066cb2bcd0249ae584a0abb05e26
fDevJc <fdevjc@gmail.com>
2022-10-21 17:07:25 +0900

    feat: 주석추가 and 카테고리,마인드셋,진행중,완료,시작일,종료일 추가

--- src/main/java/com/godlife/goalservice/service/dto/GoalServiceDto.java
+++ src/main/java/com/godlife/goalservice/service/dto/GoalServiceDto.java
@@ -2,11 +2,22 @@ package com.godlife.goalservice.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.godlife.goalservice.domain.Goal;
+import com.godlife.goalservice.domain.enums.Category;
+import com.godlife.goalservice.utils.mapper.MindsetMapper;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+/*
+    todo
+    mindsetCount, onProgressCount, completedCount 등은 API를 위해 데이터를 가공하는일인데 어디서 하는게 맞을까...
+    일단 서비스 DTO 에서 진행
+    음 또 생각해보니 서비스DTO의 역할이 너무 커지는거 같기도하고,,,,,,
+
+    - MapStruct 적용하기
+ */
 
 @Data
 @Builder
@@ -14,6 +25,15 @@ public class GoalServiceDto {
     private Long goalId;
     private String title;
     private Long userId;
+
+    private Category category;
+
+    private int mindsetCount;
+    private int onProgressCount;
+    private int completedCount;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 
     @JsonIgnore
     private List<MindsetServiceDto> mindsets;
@@ -25,6 +45,9 @@ public class GoalServiceDto {
                 .goalId(goal.getGoalId())
                 .title(goal.getTitle())
                 .userId(goal.getUserId())
+                .mindsetCount(goal.getMindsetTotalCount())
+                .onProgressCount(goal.getProgressCount())
+                .completedCount(goal.getCompletedCount())
                 .mindsets(goal.getMindsets().stream().map(MindsetServiceDto::of).collect(Collectors.toList()))
                 .todos(goal.getTodos().stream().map(TodoServiceDto::of).collect(Collectors.toList()))
                 .build();
@@ -34,8 +57,9 @@ public class GoalServiceDto {
         return Goal.builder()
                 .goalId(goalId)
                 .title(title)
+                .category(category)
                 .userId(userId)
-                .mindsets(mindsets.stream().map(MindsetServiceDto::toEntity).collect(Collectors.toList()))
+                .mindsets(mindsets.stream().map(MindsetMapper.INSTANCE::serviceDtoToEntity).collect(Collectors.toList()))
                 .todos(todos.stream().map(TodoServiceDto::toEntity).collect(Collectors.toList()))
                 .build();
     }
