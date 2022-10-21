d449b631a9d67bf03f0a9e6213e882e8d56522b0
fDevJc <fdevjc@gmail.com>
2022-10-21 20:31:02 +0900

    refactor: LocalDateTime->LocalDate 변경 및 반복 추가

--- src/main/java/com/godlife/goalservice/domain/TodoTask.java
+++ src/main/java/com/godlife/goalservice/domain/TodoTask.java
@@ -1,40 +1,49 @@
 package com.godlife.goalservice.domain;
 
+import com.godlife.goalservice.domain.converter.StringListConverter;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+import javax.persistence.Convert;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
+
 /*
     todo
     기간 어떤식으로 풀지?
     알림은 0900 String 형태가 좋을까?
  */
+
+
 @DiscriminatorValue("task")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class TodoTask extends Todo {
     @Comment("Task 시작일")
-    private LocalDateTime startDate;
+    private LocalDate startDate;
     @Comment("Task 종료일")
-    private LocalDateTime endDate;
+    private LocalDate endDate;
 
     @Comment("기간 type")
     private String repetitionType;
+
+    @Convert(converter = StringListConverter.class)
     @Comment("기간 파라미터")
-    private String repetitionParams;
+    private List<String> repetitionParams;
 
     @Comment("알림")
     private String notification;
 
     @Builder
-    public TodoTask(Long todoId, String title, Integer depth, Integer orderNumber, List<Todo> childTodos, LocalDateTime startDate, LocalDateTime endDate) {
+    public TodoTask(Long todoId, String title, Integer depth, Integer orderNumber, List<Todo> childTodos, LocalDate startDate, LocalDate endDate, String repetitionType, List<String> repetitionParams) {
         super(todoId, title, depth, orderNumber, childTodos);
         this.startDate = startDate;
         this.endDate = endDate;
+        this.repetitionType = repetitionType;
+        this.repetitionParams = repetitionParams;
     }
 }
