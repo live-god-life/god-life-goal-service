3473a9d44eb973f1dd9906f13179fa53e7779fce
fDevJc <fdevjc@gmail.com>
2022-10-26 01:22:13 +0900

    feat: TodoTask 생성시 반복 및 파람에 따라 TodoTaskSchedule 생성

--- src/main/java/com/godlife/goalservice/domain/TodoTask.java
+++ src/main/java/com/godlife/goalservice/domain/TodoTask.java
@@ -37,12 +37,52 @@ public class TodoTask extends Todo {
     @Comment("알림")
     private String notification;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "todo_id")
+    private List<TodoTaskSchedule> todoTaskSchedules = new ArrayList<>();
+
     @Builder
-    public TodoTask(Long todoId, String title, Integer depth, Integer orderNumber, List<Todo> childTodos, LocalDate startDate, LocalDate endDate, String repetitionType, List<String> repetitionParams) {
-        super(todoId, title, depth, orderNumber, childTodos);
+    public TodoTask(Long todoId, String title, Integer depth, Integer orderNumber, LocalDate startDate, LocalDate endDate, RepetitionType repetitionType, List<String> repetitionParams) {
+        super(todoId, title, depth, orderNumber);
         this.startDate = startDate;
         this.endDate = endDate;
         this.repetitionType = repetitionType;
         this.repetitionParams = repetitionParams;
+        createSchedules();
+    }
+
+    private void createSchedules() {
+        switch (repetitionType){
+            case DAY:
+                createDaySchedule();
+                break;
+            case WEEK:
+                createWeekSchedule();
+                break;
+            case MONTH:
+                createMonthSchedule();
+                break;
+        }
+    }
+
+    //TODO 뭔가 리팩토링이 필요해보인다??
+    private void createDaySchedule() {
+        for (int i = 0; i <= Period.between(startDate,endDate).getDays(); i++) {
+            todoTaskSchedules.add(new TodoTaskSchedule(startDate.plusDays(i)));
+        }
+    }
+    private void createWeekSchedule() {
+        for (int i = 0; i <= Period.between(startDate,endDate).getDays(); i++) {
+            if (repetitionParams.contains(startDate.plusDays(i).getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREA))) {
+                todoTaskSchedules.add(new TodoTaskSchedule(startDate.plusDays(i)));
+            }
+        }
+    }
+    private void createMonthSchedule() {
+        for (int i = 0; i <= Period.between(startDate,endDate).getDays(); i++) {
+            if (repetitionParams.contains(String.valueOf(startDate.plusDays(i).getDayOfMonth()))) {
+                todoTaskSchedules.add(new TodoTaskSchedule(startDate.plusDays(i)));
+            }
+        }
     }
 }
