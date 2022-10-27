7c4fa710e7041f494aeef01d06c171404670f5ab
fDevJc <fdevjc@gmail.com>
2022-10-27 21:40:27 +0900

    feat: LocalDateConverter 추가

--- src/main/java/com/godlife/goalservice/utils/config/WebMvcConfig.java
+++ src/main/java/com/godlife/goalservice/utils/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.godlife.goalservice.utils.config;
 
+import com.godlife.goalservice.utils.converter.LocalDateConverter;
 import com.godlife.goalservice.utils.converter.YearMonthConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -10,6 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new LocalDateConverter());
         registry.addConverter(new YearMonthConverter());
     }
 }
