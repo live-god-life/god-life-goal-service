15606f069d5e1575d30651181171a358f6046f6b
fDevJc <fdevjc@gmail.com>
2022-10-29 00:18:23 +0900

    feat: 투두 레파지토리 생성

---
+++ src/main/java/com/godlife/goalservice/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.godlife.goalservice.repository;
+
+import com.godlife.goalservice.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+}
