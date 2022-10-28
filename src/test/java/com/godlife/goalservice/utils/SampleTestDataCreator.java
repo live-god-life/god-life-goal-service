21bde48ab3ad714b8e2309e00ee0794955a6fbeb
fDevJc <fdevjc@gmail.com>
2022-10-29 00:26:19 +0900

    test: 필요 파라미터 추가

--- src/test/java/com/godlife/goalservice/utils/SampleTestDataCreator.java
+++ src/test/java/com/godlife/goalservice/utils/SampleTestDataCreator.java
@@ -5,10 +5,12 @@ import com.godlife.goalservice.api.request.CreateGoalTodoRequest;
 import java.util.List;
 
 public class SampleTestDataCreator {
-    public static CreateGoalTodoRequest getCreateGoalTodoTaskRequest(String title, String startDate, String endDate, String repetitionType, List<String> repetitionParams, String notification) {
+    public static CreateGoalTodoRequest getCreateGoalTodoTaskRequest(String title, Integer depth, Integer orderNumber, String startDate, String endDate, String repetitionType, List<String> repetitionParams, String notification) {
         return CreateGoalTodoRequest.builder()
                 .type("task")
                 .title(title)
+                .depth(depth)
+                .orderNumber(orderNumber)
                 .startDate(startDate)
                 .endDate(endDate)
                 .repetitionType(repetitionType)
@@ -17,9 +19,11 @@ public class SampleTestDataCreator {
                 .build();
     }
 
-    public static CreateGoalTodoRequest getCreateGoalTodoFolderRequest(String title, List<CreateGoalTodoRequest> childTodoRequest) {
+    public static CreateGoalTodoRequest getCreateGoalTodoFolderRequest(String title, Integer depth, Integer orderNumber, List<CreateGoalTodoRequest> childTodoRequest) {
         return CreateGoalTodoRequest.builder()
                 .type("folder")
+                .depth(depth)
+                .orderNumber(orderNumber)
                 .title(title)
                 .todos(childTodoRequest)
                 .build();
