2c2f2e3a14bc01ed21e0c7d26cf2692fe6b4177b
fDevJc <fdevjc@gmail.com>
2022-11-03 15:19:48 +0900

    feat: error response 팩토리 메소드 추가

--- src/main/java/com/godlife/goalservice/dto/response/ApiResponse.java
+++ src/main/java/com/godlife/goalservice/dto/response/ApiResponse.java
@@ -12,29 +12,33 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class ApiResponse {
-    private String status;
-    private String message;
-    private Object data;
+	private String status;
+	private String message;
+	private Object data;
 
-    private ApiResponse(String status, String message, Object data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
+	private ApiResponse(String status, String message, Object data) {
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
 
-    public static ApiResponse createGetSuccessResponse(List dtos) {
-        return new ApiResponse("success", "ok", dtos);
-    }
+	public static ApiResponse createGetSuccessResponse(List dtos) {
+		return new ApiResponse("success", "ok", dtos);
+	}
 
-    public static ApiResponse createPostSuccessResponse() {
-        return new ApiResponse("success", "created", null);
-    }
+	public static ApiResponse createPostSuccessResponse() {
+		return new ApiResponse("success", "created", null);
+	}
 
-    public static ApiResponse createPatchSuccessResponse() {
-        return new ApiResponse("success", "modified", null);
-    }
+	public static ApiResponse createPatchSuccessResponse() {
+		return new ApiResponse("success", "modified", null);
+	}
 
-    public static ApiResponse createGetSuccessResponse(Object o) {
-        return new ApiResponse("success", "ok", o);
-    }
+	public static ApiResponse createGetSuccessResponse(Object o) {
+		return new ApiResponse("success", "ok", o);
+	}
+
+	public static ApiResponse createErrorResponse(String errorMessage) {
+		return new ApiResponse("error", errorMessage, null);
+	}
 }
